af2d72f365f87c7ba26797ae5ac7afa49289b61f
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2025-10-16 00:55:29 -0300

    Updated SecondaryIncomeForm.tsx

--- src/components/SecondaryIncomeForm.tsx
+++ src/components/SecondaryIncomeForm.tsx
@@ -8,8 +8,7 @@ export default function SecondaryIncomeForm({ onContinue, formData, questionNumb
   const [selectedSources, setSelectedSources] = useState<string[]>(formData?.secondaryIncomeSources || []);
   const [otherSegundaryIncomeSource, setotherSegundaryIncomeSource] = useState(formData?.otherSegundaryIncomeSource || '');
   const [secondaryIncomeValue, setSecondaryIncomeValue] = useState(formData?.secondaryIncomeValue || '');
-  const [displayValue, setDisplayValue] = useState(
-  formData?.secondaryIncomeValue ? formatCurrencyInput(formData.secondaryIncomeValue) : '');
+  const [displayValue, setDisplayValue] = useState(formData?.secondaryIncomeValue ? formatCurrencyInput(formData.secondaryIncomeValue) : '');
 
 
   const incomeOptions = [
