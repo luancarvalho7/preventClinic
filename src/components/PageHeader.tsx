346221cb6f663d744380186239cd274a3f6a2b14
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2026-01-22 09:02:23 -0300

    Updated PageHeader.tsx

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,7 +3,7 @@ import Logo from './Logo';
 
 export default function PageHeader() {
   return (
-    <div className="bg-gradient-to-br">
+    <div className="bg-gradient-to-br from-slate-50 to-slate-100">
       <div className="max-w-2xl mx-auto px-6 py-4">
         <Logo height="h-5" />
       </div>
